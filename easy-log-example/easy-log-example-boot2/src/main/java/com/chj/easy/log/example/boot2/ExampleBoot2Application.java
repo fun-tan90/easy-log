19d65fd1c0d8cbd2aa729ae1d0e16c279aaadae5
陈浩杰 <897546244@qq.com>
2023-07-13 18:18:40 +0800

    init

---
+++ easy-log-example/easy-log-example-boot2/src/main/java/com/chj/easy/log/example/boot2/ExampleBoot2Application.java
@@ -0,0 +1,22 @@
+package com.chj.easy.log.example.boot2;
+
+import com.chj.easy.log.common.constant.EasyLogConstants;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * description TODO
+ * company 铁人科技
+ *
+ * @author 陈浩杰
+ * @date 2023/7/13 8:12
+ */
+@SpringBootApplication(scanBasePackages = EasyLogConstants.SCAN_BASE_PACKAGES)
+public class ExampleBoot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleBoot2Application.class, args);
+    }
+
+}
