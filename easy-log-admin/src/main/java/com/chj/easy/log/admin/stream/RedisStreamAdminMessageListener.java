dcd522046ffd1eaade207ddea5c568013f35eb6a
陈浩杰 <897546244@qq.com>
2023-07-18 22:06:09 +0800

    feat:新增滑动窗口

--- easy-log-admin/src/main/java/com/chj/easy/log/admin/stream/RedisStreamAdminMessageListener.java
+++ easy-log-admin/src/main/java/com/chj/easy/log/admin/stream/RedisStreamAdminMessageListener.java
@@ -2,7 +2,9 @@ package com.chj.easy.log.admin.stream;
 
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
+import com.chj.easy.log.common.EasyLogManager;
 import com.chj.easy.log.common.constant.EasyLogConstants;
+import com.chj.easy.log.common.window.SlidingWindow;
 import lombok.extern.slf4j.Slf4j;
 import net.dreamlu.iot.mqtt.codec.MqttQoS;
 import net.dreamlu.iot.mqtt.spring.server.MqttServerTemplate;
@@ -16,6 +18,7 @@ import org.springframework.util.StringUtils;
 import javax.annotation.Resource;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 
 /**
@@ -29,6 +32,8 @@ import java.util.*;
 @Component
 public class RedisStreamAdminMessageListener implements StreamListener<String, MapRecord<String, String, String>> {
 
+    public static final SlidingWindow SLIDING_WINDOW = new SlidingWindow(1000, 5, 8);
+
     @Resource
     StringRedisTemplate stringRedisTemplate;
 
@@ -40,55 +45,81 @@ public class RedisStreamAdminMessageListener implements StreamListener<String, M
         if (entries != null) {
             String recordId = entries.getId().getValue();
             Map<String, String> logMap = entries.getValue();
-            try {
-                Set<String> clientIds = stringRedisTemplate.opsForSet().members(EasyLogConstants.MQTT_ONLINE_CLIENTS);
-                if (CollectionUtils.isEmpty(clientIds)) {
-                    return;
-                }
-                Iterator<String> clientIdsIterator = clientIds.iterator();
-                while (clientIdsIterator.hasNext()) {
-                    String clientId = clientIdsIterator.next();
-                    String realTimeFilterRulesStr = stringRedisTemplate.opsForValue().get(EasyLogConstants.REAL_TIME_FILTER_RULES + clientId);
-                    if (StringUtils.hasLength(realTimeFilterRulesStr)) {
-                        JSONObject realTimeFilterRules = JSONUtil.parseObj(realTimeFilterRulesStr);
-                        for (String realTimeFilterRule : realTimeFilterRules.keySet()) {
-                            String[] split = realTimeFilterRule.split("#");
-                            String ruleKey = split[0];
-                            String ruleWay = split[1];
-                            String logVal = logMap.get(ruleKey);
-                            String ruleVal = realTimeFilterRules.getStr(realTimeFilterRule);
-                            if ("eq".equals(ruleWay)) {
-                                if (!logVal.equals(ruleVal)) {
-                                    clientIdsIterator.remove();
-                                    break;
-                                }
-                            } else if ("should".equals(ruleWay)) {
-                                List<String> list = Arrays.asList(ruleVal.split("%"));
-                                Optional<String> any = list.stream().filter(logVal::contains).findAny();
-                                if (!any.isPresent()) {
-                                    clientIdsIterator.remove();
-                                    break;
-                                }
-                            } else if ("gle".equals(ruleWay)) {
-                                if (Long.parseLong(ruleVal) > Long.parseLong(logVal)) {
-                                    clientIdsIterator.remove();
-                                    break;
-                                }
+            CompletableFuture<Void> cfAll = CompletableFuture.allOf(logAlarm(logMap), logRealTimeFilter(logMap));
+            cfAll.thenAccept(r -> {
+                stringRedisTemplate.opsForStream().acknowledge(EasyLogConstants.STREAM_KEY, EasyLogConstants.GROUP_ADMIN_NAME, recordId);
+            }).exceptionally(e -> {
+                e.printStackTrace();
+                stringRedisTemplate.opsForStream().acknowledge(EasyLogConstants.STREAM_KEY, EasyLogConstants.GROUP_ADMIN_NAME, recordId);
+                return null;
+            });
+        }
+    }
+
+    /**
+     * 日志告警
+     *
+     * @param logMap
+     */
+    private CompletableFuture<Void> logAlarm(Map<String, String> logMap) {
+        return CompletableFuture.runAsync(() -> {
+            boolean res = SLIDING_WINDOW.addCount(1);
+            log.info("{}超过阈值", res ? "已" : "未");
+        }, EasyLogManager.EASY_LOG_FIXED_THREAD_POOL);
+    }
+
+    /**
+     * 实时日志过滤
+     *
+     * @param logMap
+     */
+    private CompletableFuture<Void> logRealTimeFilter(Map<String, String> logMap) {
+        return CompletableFuture.runAsync(() -> {
+            Set<String> clientIds = stringRedisTemplate.opsForSet().members(EasyLogConstants.MQTT_ONLINE_CLIENTS);
+            if (CollectionUtils.isEmpty(clientIds)) {
+                return;
+            }
+            Iterator<String> clientIdsIterator = clientIds.iterator();
+            while (clientIdsIterator.hasNext()) {
+                String clientId = clientIdsIterator.next();
+                String realTimeFilterRulesStr = stringRedisTemplate.opsForValue().get(EasyLogConstants.REAL_TIME_FILTER_RULES + clientId);
+                if (StringUtils.hasLength(realTimeFilterRulesStr)) {
+                    JSONObject realTimeFilterRules = JSONUtil.parseObj(realTimeFilterRulesStr);
+                    for (String realTimeFilterRule : realTimeFilterRules.keySet()) {
+                        String[] split = realTimeFilterRule.split("#");
+                        String ruleKey = split[0];
+                        String ruleWay = split[1];
+                        String logVal = logMap.get(ruleKey);
+                        String ruleVal = realTimeFilterRules.getStr(realTimeFilterRule);
+                        if ("eq".equals(ruleWay)) {
+                            if (!logVal.equals(ruleVal)) {
+                                clientIdsIterator.remove();
+                                break;
+                            }
+                        } else if ("should".equals(ruleWay)) {
+                            List<String> list = Arrays.asList(ruleVal.split("%"));
+                            Optional<String> any = list.stream().filter(logVal::contains).findAny();
+                            if (!any.isPresent()) {
+                                clientIdsIterator.remove();
+                                break;
+                            }
+                        } else if ("gle".equals(ruleWay)) {
+                            if (Long.parseLong(ruleVal) > Long.parseLong(logVal)) {
+                                clientIdsIterator.remove();
+                                break;
                             }
                         }
-                    } else {
-                        clientIdsIterator.remove();
                     }
+                } else {
+                    clientIdsIterator.remove();
                 }
-                if (!CollectionUtils.isEmpty(clientIds)) {
-                    byte[] logBytes = JSONUtil.toJsonStr(logMap).getBytes(StandardCharsets.UTF_8);
-                    for (String clientId : clientIds) {
-                        mqttServerTemplate.publish(clientId, EasyLogConstants.AFTER_FILTER_TOPIC, logBytes, MqttQoS.AT_MOST_ONCE);
-                    }
-                }
-            } finally {
-                stringRedisTemplate.opsForStream().acknowledge(EasyLogConstants.STREAM_KEY, EasyLogConstants.GROUP_ADMIN_NAME, recordId);
             }
-        }
+            if (!CollectionUtils.isEmpty(clientIds)) {
+                byte[] logBytes = JSONUtil.toJsonStr(logMap).getBytes(StandardCharsets.UTF_8);
+                for (String clientId : clientIds) {
+                    mqttServerTemplate.publish(clientId, EasyLogConstants.AFTER_FILTER_TOPIC, logBytes, MqttQoS.AT_MOST_ONCE);
+                }
+            }
+        }, EasyLogManager.EASY_LOG_FIXED_THREAD_POOL);
     }
 }
---
+++ easy-log-common/src/main/java/com/chj/easy/log/common/window/SystemClock.java
@@ -0,0 +1,58 @@
+package com.chj.easy.log.common.window;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * description 用于解决高并发下System.currentTimeMillis卡顿
+ * company 铁人科技
+ *
+ * @author 陈浩杰
+ * @date 2023/7/18 22:05
+ */
+public class SystemClock {
+
+    private final int period;
+
+    private final AtomicLong now;
+
+    private static class InstanceHolder {
+        private static final SystemClock INSTANCE = new SystemClock(1);
+    }
+
+    private SystemClock(int period) {
+        this.period = period;
+        this.now = new AtomicLong(System.currentTimeMillis());
+        scheduleClockUpdating();
+    }
+
+    private static SystemClock instance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    private void scheduleClockUpdating() {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable runnable) {
+                Thread thread = new Thread(runnable, "System Clock");
+                thread.setDaemon(true);
+                return thread;
+            }
+        });
+        scheduler.scheduleAtFixedRate(() -> now.set(System.currentTimeMillis()), period, period, TimeUnit.MILLISECONDS);
+    }
+
+    private long currentTimeMillis() {
+        return now.get();
+    }
+
+    /**
+     * 用来替换原来的System.currentTimeMillis()
+     */
+    public static long now() {
+        return instance().currentTimeMillis();
+    }
+}
