8f6ad5a76799674fd3b7067951aa65b89deefdaa
陈浩杰 <897546244@qq.com>
2023-07-19 18:16:00 +0800

    feat:新增索引管理接口

--- easy-log-admin/src/main/java/com/chj/easy/log/admin/rest/SysController.java
+++ easy-log-admin/src/main/java/com/chj/easy/log/admin/rest/SysController.java
@@ -7,6 +7,7 @@ import com.chj.easy.log.core.service.EsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -28,7 +29,7 @@ public class SysController {
     EsService esService;
 
     @GetMapping("index-management")
-    public Res<List<IndexManagementVo>> indexManagement() {
-        return Res.ok(esService.indexManagement("daily-easy-log-*"));
+    public Res<List<IndexManagementVo>> indexManagement(@RequestParam(defaultValue = "daily-easy-log-*", required = false) String indexNamePattern) {
+        return Res.ok(esService.indexManagement(indexNamePattern));
     }
 }
