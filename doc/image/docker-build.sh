a967cb2485941a448e4727500d96a237a74a3011
陈浩杰 <897546244@qq.com>
2023-07-28 15:13:30 +0800

    build:添加docker-maven-plugin和maven-antrun-plugin插件

--- doc/image/docker-build.sh
+++ doc/image/docker-build.sh
@@ -4,5 +4,6 @@
 # -t 后面为镜像名称和TAG
 docker build --build-arg PROJECT_NAME=easy-log-server --build-arg SERVER_PORT=1203 --build-arg MQTT_TCP_PORT=1883 --build-arg MQTT_WEBSOCKET_PORT=8083 . -t registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log-server:1.1.1
 docker tag registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log-server:1.1.1 registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log-server:latest
+docker login --username=陈浩杰1991 --password qingquaner151628  registry.cn-hangzhou.aliyuncs.com
 docker push registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log-server:1.1.1
 docker push registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log-server:latest
