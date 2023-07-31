d7f189297d75cb0458cbaf2e5970078a89d05ff1
陈浩杰 <897546244@qq.com>
2023-07-31 17:52:50 +0800

    fix:修复cookie无法读取token问题

--- easy-log-module/easy-log-admin/src/main/java/com/chj/easy/log/admin/rest/AuthController.java
+++ easy-log-module/easy-log-admin/src/main/java/com/chj/easy/log/admin/rest/AuthController.java
@@ -17,9 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+
 /**
- * @author chj
- * @date 2021年07月17日 16:38
+ * description TODO
+ * company 铁人科技
+ *
+ * @author 陈浩杰
+ * @date 2023/7/31 17:52
  */
 @Slf4j
 @RestController
