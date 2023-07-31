9e303f224b05bc1180c613f2936d3a81a8d00cba
陈浩杰 <897546244@qq.com>
2023-07-31 21:26:09 +0800

    build: docker构建优化

--- doc/image/docker-build.sh
+++ doc/image/docker-build.sh
@@ -2,8 +2,9 @@
 # 构建过程变量SERVER_PORT、PROJECT_NAME、MQTT_TCP_PORT、MQTT_WEBSOCKET_PORT
 # 启动时变量HEAP_SIZE、SPRING_PROFILES_ACTIVE
 # -t 后面为镜像名称和TAG
-docker build --build-arg PROJECT_NAME=easy-log-server --build-arg SERVER_PORT=1203 --build-arg MQTT_TCP_PORT=1883 --build-arg MQTT_WEBSOCKET_PORT=8083 . -t registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log-server:1.1.1
-docker tag registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log-server:1.1.1 registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log-server:latest
+tag="1.1.2"
+docker build --build-arg PROJECT_NAME=easy-log-server --build-arg SERVER_PORT=1203 --build-arg MQTT_TCP_PORT=1883 --build-arg MQTT_WEBSOCKET_PORT=8083 . -t registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log:$tag
+docker tag registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log:$tag registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log:latest
 docker login --username=陈浩杰1991 --password qingquaner151628  registry.cn-hangzhou.aliyuncs.com
-docker push registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log-server:1.1.1
-docker push registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log-server:latest
+docker push registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log:$tag
+docker push registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log:latest
