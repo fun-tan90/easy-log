a4c982123e79a8caaa6ae5bdacf7518d36efc334
陈浩杰 <897546244@qq.com>
2023-08-01 10:09:24 +0800

    style: 优化banner

--- doc/image/docker-build.sh
+++ doc/image/docker-build.sh
@@ -3,8 +3,10 @@
 # 启动时变量HEAP_SIZE、SPRING_PROFILES_ACTIVE
 # -t 后面为镜像名称和TAG
 tag="1.1.3"
-docker build --build-arg PROJECT_NAME=easy-log-server --build-arg SERVER_PORT=1203 --build-arg MQTT_TCP_PORT=1883 --build-arg MQTT_WEBSOCKET_PORT=8083 . -t registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log:$tag
-docker tag registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log:$tag registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log:latest
+imageName="registry.cn-hangzhou.aliyuncs.com/tan90/easy-log"
+
+docker build --build-arg PROJECT_NAME=easy-log-server --build-arg SERVER_PORT=1203 --build-arg MQTT_TCP_PORT=1883 --build-arg MQTT_WEBSOCKET_PORT=8083 . -t $imageName:$tag
+docker tag $imageName:$tag $imageName:latest
 docker login --username=陈浩杰1991 --password qingquaner151628  registry.cn-hangzhou.aliyuncs.com
-docker push registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log:$tag
-docker push registry.cn-hangzhou.aliyuncs.com/chenhj/easy-log:latest
+docker push $imageName:$tag
+docker push $imageName:latest
