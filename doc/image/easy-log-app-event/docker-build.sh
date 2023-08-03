c2e672c047fb8d90c780f029ca54339744db5a62
陈浩杰 <897546244@qq.com>
2023-08-03 11:06:28 +0800

    feat: Dockerfile打包脚本修改

--- doc/image/easy-log-app-event/docker-build.sh
+++ doc/image/easy-log-app-event/docker-build.sh
@@ -3,9 +3,9 @@
 # 启动时变量HEAP_SIZE、SPRING_PROFILES_ACTIVE
 # -t 后面为镜像名称和TAG
 tag="1.1.5"
-imageName="registry.cn-hangzhou.aliyuncs.com/tan90/easy-log"
+imageName="registry.cn-hangzhou.aliyuncs.com/tan90/easy-log-app-event"
 
-docker build --build-arg PROJECT_NAME=easy-log-server --build-arg SERVER_PORT=1203 --build-arg MQTT_TCP_PORT=1883 --build-arg MQTT_WEBSOCKET_PORT=8083 . -t $imageName:$tag
+docker build --build-arg PROJECT_NAME=easy-log-app-event --build-arg SERVER_PORT=1203 --build-arg MQTT_TCP_PORT=1883 --build-arg MQTT_WEBSOCKET_PORT=8083 . -t $imageName:$tag
 docker tag $imageName:$tag $imageName:latest
 docker login --username=陈浩杰1991 --password qingquaner151628  registry.cn-hangzhou.aliyuncs.com
 docker push $imageName:$tag
