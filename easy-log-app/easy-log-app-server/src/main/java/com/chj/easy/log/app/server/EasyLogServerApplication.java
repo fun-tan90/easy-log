a880c61fd265d385741cc3ae53d3c87f407c4769
陈浩杰 <897546244@qq.com>
2023-08-03 09:14:18 +0800

    feat: 计算模块各类规则采取一级缓存方式

---
+++ easy-log-app/easy-log-app-server/src/main/java/com/chj/easy/log/app/server/EasyLogServerApplication.java
@@ -0,0 +1,21 @@
+package com.chj.easy.log.app.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * description TODO
+ * company 铁人科技
+ *
+ * @author 陈浩杰
+ * @date 2023/7/13 8:12
+ */
+@SpringBootApplication
+public class EasyLogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyLogServerApplication.class, args);
+    }
+
+}
