5d470a6647e4f2351b26c13fc743682be91612cc
陈浩杰 <897546244@qq.com>
2023-08-24 17:29:20 +0800

    feat: 更新前端静态资源

--- easy-log-server/easy-log-app/src/main/resources/static/assets/LoginFormTitle-cb639bc3.js
+++ easy-log-server/easy-log-app/src/main/resources/static/assets/LoginFormTitle-cb639bc3.js
@@ -1,1 +1,1 @@
-import{_ as o}from "./LoginFormTitle.vue_vue_type_script_setup_true_lang-64884d13.js";import"./index.js";import"./vue-50936e44.js";import"./antd-685f22a1.js";export{o as default};
+import{_ as o}from"./LoginFormTitle.vue_vue_type_script_setup_true_lang-64884d13.js";import"./index.js";import"./vue-50936e44.js";import"./antd-685f22a1.js";export{o as default};
